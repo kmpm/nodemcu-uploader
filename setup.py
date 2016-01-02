37004fad7758653be3c465a668570009e959b6b9
Roman Chyla <roman.chyla@gmail.com>
2016-01-01 20:55:16 -0500

    Changing dependency version (pyserial most recent release 3.0; won't work)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='nodemcu-uploader',
       version='0.1.0-beta',
       install_requires=[
-          'pyserial>=3.0'
+          'pyserial==2.7'
       ],
       scripts = ['nodemcu-uploader.py'],
       url='https://github.com/kmpm/nodemcu-uploader',
