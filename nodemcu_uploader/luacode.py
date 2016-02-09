7edc592d96b4d0ec7eeb7e1fb95d8593c4659a0d
kmpm <peter@birchroad.net>
2016-02-09 16:49:42 +0100

    use the new crypto.fhash
    * should help #40 when the neccesary code
      gets included in the firmware

--- nodemcu_uploader/luacode.py
+++ nodemcu_uploader/luacode.py
@@ -32,7 +32,7 @@ function recv_block(d)
 end
 function recv_name(d) d = string.gsub(d, '\000', '') file.remove(d) file.open(d, 'w') uart.on('data', 130, recv_block, 0) uart.write(0, '\006') end
 function recv() uart.on('data', '\000', recv_name, 0) uart.write(0, 'C') end
-function shafile(f) file.open(f, "r") print(crypto.toHex(crypto.hash("sha1",file.read()))) file.close() end
+function shafile(f) crypto.toHex(crypto.fhash('sha1', f)) end
 """
 
 SEND_LUA = \
