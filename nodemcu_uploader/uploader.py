fd196c59a51e6b770da20159d687914e83f13f60
kmpm <peter@birchroad.net>
2016-07-29 20:25:19 +0200

    Increased format timeout
    * increased timeout to 5 minutes
    * a better, more general, message about how long time it may
      take to format the filesystem
    
    Closes #56

--- nodemcu_uploader/uploader.py
+++ nodemcu_uploader/uploader.py
@@ -421,8 +421,8 @@ class Uploader(object):
 
     def file_format(self):
         """Formats device filesystem"""
-        log.info('Formating, can take up to 1 minute...')
-        res = self.__exchange('file.format()', timeout=60)
+        log.info('Formating, can take minutes depending on flash size...')
+        res = self.__exchange('file.format()', timeout=300)
         if 'format done' not in res:
             log.error(res)
         else:
