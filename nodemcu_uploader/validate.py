f2193d87c12ace46996fff490159e1784d3c26fb
peter <git@kmpm.se>
2019-12-12 22:19:31 +0100

    prettier validation of path length

--- nodemcu_uploader/validate.py
+++ nodemcu_uploader/validate.py
@@ -1,4 +1,4 @@
-from .exceptions import PathLengthException
+from .exceptions import ValidationException
 
 MAX_FS_NAME_LEN = 31
 
@@ -8,6 +8,6 @@ def remotePath(path):
     Raises exception if not valid
     """
     if len(path) > MAX_FS_NAME_LEN:
-        raise PathLengthException
+        raise ValidationException('To long. >{0}'.format(MAX_FS_NAME_LEN), 'path', path)
     if len(path) < 1:
-        raise PathLengthException
+        raise ValidationException('To short', 'path', path)
