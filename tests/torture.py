061977b247582f4e3d99fe0825df5a229c939b25
peter <git@kmpm.se>
2019-12-12 22:19:57 +0100

    test some more files

--- tests/torture.py
+++ tests/torture.py
@@ -30,32 +30,44 @@ class TestTorture(unittest.TestCase):
     uploader = None
 
     def setUp(self):
+        log.debug("setUp")
         self.uploader = Uploader(SERIALPORT)
 
     def tearDown(self):
+        log.debug("tearDown")
         if is_real():
             self.uploader.node_restart()
         self.uploader.close()
         time.sleep(1)
 
     def task_upload_verify_compile(self):
-        print('upload-verify-compile')
+        """Upload lua code, verify and compile"""
+        log.debug('upload-verify-compile')
         self.assertTrue(self.uploader.prepare())
         dests = operation_upload(self.uploader, "tests/fixtures/*.lua", 'sha1', True, False, False)
         return len(dests)
 
+    def task_upload_verify(self):
+        """Upload some text files and verify"""
+        log.debug('upload-verify')
+        dests = operation_upload(self.uploader, "tests/fixtures/*_file.txt", 'sha1', False, False, False)
+        return len(dests)
+
     def task_check_remote_files(self, wanted):
+        """Check that the wanted number of files exists on the device"""
         lst = self.uploader.file_list()
         self.assertIsInstance(lst, type([]))
         self.assertEqual(len(lst), wanted)
         return lst
 
     def task_remove_all_files(self):
-        print('remove all files')
+        """Remove all files on device"""
+        log.debug('remove all files')
         self.uploader.file_remove_all()
 
     def task_download_all_files(self, files):
-        print('download all files', files)
+        """Downloads all files on device and do a sha1 checksum"""
+        log.debug('download all files and verify. %s', files)
         dest = os.path.join('.', 'tmp')
         operation_download(self.uploader, files, dest=dest)
         for f in files:
@@ -64,18 +76,23 @@ class TestTorture(unittest.TestCase):
             self.uploader.verify_file(local, f, 'sha1')
 
     def task_remove_tmp(self):
+        """Removes local tmp folder"""
         dest = os.path.join('.', 'tmp')
         if os.path.isdir(dest):
             shutil.rmtree(dest)
 
     def test_for_long_time(self):
-        for x in range(20):
-            print('{x} test sequence'.format(x=x+1))
+        testcount = 2
+        for x in range(testcount):
+            print('test sequence {0}/{1}'.format(x+1, testcount))
+            log.info('--- test sequence {0}/{1} ---'.format(x+1, testcount))
             self.task_remove_tmp()
             self.task_remove_all_files()
             self.task_check_remote_files(0)
             time.sleep(0.5)
             count = self.task_upload_verify_compile()
             self.assertEqual(count, 3)
+            count += self.task_upload_verify()
+            self.assertEqual(count, 6)
             files = self.task_check_remote_files(count)
             self.task_download_all_files(list(map(lambda x: x[0], files)))
